3cc55207788b58f0b9682d3c5623c55d0d98c16d
VladimirLashmanov <vov7las@gmail.com>
2023-02-15 15:19:14 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Block} from './Block';
+import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [fromCurrency, setFromCurrency] = React.useState('UAH')
+    const [toCurrency, setToCurrency] = React.useState('USD')
+    const [fromPrice, setFromPrice] = React.useState(0)
+    const [toPrice, setToPrice] = React.useState(1)
+
+
+    // const [rates, setRates] = React.useState({})
+
+    const ratesRef=React.useRef({})
+    React.useEffect(() => {
+        fetch('https://www.cbr-xml-daily.ru/latest.js')
+            .then((res) => res.json())
+            .then((json) => {
+                // setRates(json.rates)
+                ratesRef.current=json.rates
+                onChangeToPrice(1)
+
+            }).catch(err => {
+            console.warn(err)
+            alert('no informations')
+        })
+    }, [])
+
+    const onChangeFromPrice = (value) => {
+        const price = value / ratesRef.current[fromCurrency];
+        const result = price * ratesRef.current[toCurrency]
+        setToPrice(result.toFixed(3))
+        setFromPrice(value)
+
+    }
+    const onChangeToPrice = (value) => {
+        const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+        setFromPrice(result.toFixed(3))
+        setToPrice(value)
+    }
+
+
+    React.useEffect(()=>{
+onChangeFromPrice(fromPrice)
+    },[fromCurrency])
+
+    React.useEffect(()=>{
+onChangeToPrice(toPrice)
+    },[toCurrency])
+
+
+
+    return (
+        <div className="App">
+            <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency}
+                   onChangeValue={onChangeFromPrice}/>
+            <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency}
+                   onChangeValue={onChangeToPrice}/>
+
+        </div>
+    );
 }
 
 export default App;
